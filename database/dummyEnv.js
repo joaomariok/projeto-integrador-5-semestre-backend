96d14eabde370cf240da3c619648626580c48046
joao-vgarcia <joao.vgarcia10@gmail.com>
2022-06-05 18:08:03 -0300

    set dummy env

---
+++ database/dummyEnv.js
@@ -0,0 +1,11 @@
+module.exports = {
+  MYSQL_HOST: process.env.DB_HOST,
+  MYSQL_USER: process.env.DB_USER,
+  MYSQL_PASS: process.env.DB_PASS,
+  MYSQL_NAME: process.env.DB_NAME,
+  MYSQL_PORT:process.env.DB_PORT,
+  ROOT_USERNAME:process.env.ROOT_USERNAME,
+  ROOT_PASSWORD:process.env.ROOT_PASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET,
+  DEBUG: 'false',
+};
