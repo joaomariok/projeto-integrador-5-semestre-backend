82c8f1575ae88841b7b783378c4416c506054efd
joao-vgarcia <joao.vgarcia10@gmail.com>
2022-06-05 17:02:39 -0300

    remove comments

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,14 @@
-const { sleep } = require("./helper");
-const database = require('.');
-const md5 = require("md5");
-
 // Import models
 const Paciente = require('../models/paciente');
 const Atendimento = require('../models/atendimento');
 const Prontuario = require('../models/prontuario');
 const Usuario = require("../models/usuario");
 const dummyData = require("./dummydata");
-// const keys = require("./keys");
-// const env = require("./env");
+const keys = require("./keys");
+const env = require("./env");
+const { sleep } = require("./helper");
+const database = require('.');
+const md5 = require("md5");
 
 // Constants
 const RETRY_CONNECTION_TIMER = 10 * 1e3;
@@ -28,79 +27,79 @@ async function isDatabaseConnected() {
     return isConnected;
 }
 
-// async function createRootUser() {
-//     console.log(env.ROOT_USERNAME);
-//     const userInDatabase = await Usuario.findOne({
-//         where: {
-//             username: env.ROOT_USERNAME,
-//         }
-//     });
+async function createRootUser() {
+    console.log(env.ROOT_USERNAME);
+    const userInDatabase = await Usuario.findOne({
+        where: {
+            username: env.ROOT_USERNAME,
+        }
+    });
 
-//     if (Boolean(userInDatabase)) return;
+    if (Boolean(userInDatabase)) return;
 
-//     const rootUser = await Usuario.create({
-//         username: env.ROOT_USERNAME,
-//         password: md5(env.ROOT_PASSWORD),
-//     });
-// }
+    const rootUser = await Usuario.create({
+        username: env.ROOT_USERNAME,
+        password: md5(env.ROOT_PASSWORD),
+    });
+}
 
-// async function createDummyEntry(_paciente, _prontuario, _atendimento) {
-//     const paciente = await Paciente.create({
-//         idade: _paciente.idade,
-//         genero: _paciente.genero,
-//         multiplicidade: _paciente.multiplicidade
-//     });
+async function createDummyEntry(_paciente, _prontuario, _atendimento) {
+    const paciente = await Paciente.create({
+        idade: _paciente.idade,
+        genero: _paciente.genero,
+        multiplicidade: _paciente.multiplicidade
+    });
 
-//     const prontuario = await Prontuario.create({
-//         paciente_id: paciente.id,
-//         sintomas: _prontuario.sintomas,
-//         veioAObito: _prontuario.veioAObito,
-//         transferencia: _prontuario.transferencia,
-//         gravidade: _prontuario.gravidade,
-//         vacinadoCovid: _prontuario.vacinadoCovid,
-//         possuiComorbidades: _prontuario.possuiComorbidades
-//     });
+    const prontuario = await Prontuario.create({
+        paciente_id: paciente.id,
+        sintomas: _prontuario.sintomas,
+        veioAObito: _prontuario.veioAObito,
+        transferencia: _prontuario.transferencia,
+        gravidade: _prontuario.gravidade,
+        vacinadoCovid: _prontuario.vacinadoCovid,
+        possuiComorbidades: _prontuario.possuiComorbidades
+    });
 
-//     const atendimento = Atendimento.create({
-//         horaEntrada: _atendimento.horaEntrada.replace("T", " "),
-//         horaSaida: _atendimento.horaSaida.replace("T", " "),
-//         paciente_id: paciente.id,
-//         prontuario_id: prontuario.id
-//     });
-// }
+    const atendimento = Atendimento.create({
+        horaEntrada: _atendimento.horaEntrada.replace("T", " "),
+        horaSaida: _atendimento.horaSaida.replace("T", " "),
+        paciente_id: paciente.id,
+        prontuario_id: prontuario.id
+    });
+}
 
-// async function createDummyData() {
-//     dummyData.forEach(async (registro, index) => {
-//         console.log(`[DB] Creating dummy entry ${index + 1}/${dummyData.length}`);
-//         await createDummyEntry(registro.paciente, registro.prontuario, registro.atendimento);
-//     });
-// }
+async function createDummyData() {
+    dummyData.forEach(async (registro, index) => {
+        console.log(`[DB] Creating dummy entry ${index + 1}/${dummyData.length}`);
+        await createDummyEntry(registro.paciente, registro.prontuario, registro.atendimento);
+    });
+}
 
 // Connection to database
-// async function trySyncDatabase() {
-//     try {
-//         const resultado = await database.sync({ force: keys.isDebug });
-//         console.log("[DB] Database synchronized!");
-//         console.log(resultado.models);
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
+async function trySyncDatabase() {
+    try {
+        const resultado = await database.sync({ force: keys.isDebug });
+        console.log("[DB] Database synchronized!");
+        console.log(resultado.models);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 async function tryDatabaseConnection() {
     for (retryNumber = 0; retryNumber < MAX_CONNECTION_RETRY_COUNT; retryNumber++) {
         try {
             await database.authenticate();
             console.log("[DB] Connected to database!");
-            // await trySyncDatabase();
+            await trySyncDatabase();
             isConnected = true;
-            // await createRootUser();
+            await createRootUser();
 
-            // if (keys.isDebug) {
-            //     console.log("[DB] Creating dummy data");
-            //     await createDummyData();
-            //     console.log("[DB] Finished creating dummy data");
-            // }
+            if (keys.isDebug) {
+                console.log("[DB] Creating dummy data");
+                await createDummyData();
+                console.log("[DB] Finished creating dummy data");
+            }
         } catch (err) {
             console.log(`[DB] Retrying connection to database: ${retryNumber}/${MAX_CONNECTION_RETRY_COUNT}`);
             console.log(process.env.DB_HOST)
@@ -114,5 +113,4 @@ async function tryDatabaseConnection() {
     }
 }
 
-// module.exports = { isDatabaseConnected, tryDatabaseConnection, createDummyData };
-module.exports = { isDatabaseConnected, tryDatabaseConnection };
+module.exports = { isDatabaseConnected, tryDatabaseConnection, createDummyData };
--- routes.js
+++ routes.js
@@ -1,17 +1,17 @@
 const { Router } = require("express");
 const env = require("./database/env");
-// const md5 = require("md5");
-// const { sign } = require("jsonwebtoken");
+const md5 = require("md5");
+const { sign } = require("jsonwebtoken");
 
-// const { ensureAuthenticated } = require("./middleware/ensureAuthenticated");
-// const { isDatabaseConnected } = require('./database/connection');
+const { ensureAuthenticated } = require("./middleware/ensureAuthenticated");
+const { isDatabaseConnected } = require('./database/connection');
 
 // Import models
-// const database = require('./database');
-// const Paciente = require('./models/paciente');
-// const Atendimento = require('./models/atendimento');
-// const Prontuario = require('./models/prontuario');
-// const Usuario = require("./models/usuario");
+const database = require('./database');
+const Paciente = require('./models/paciente');
+const Atendimento = require('./models/atendimento');
+const Prontuario = require('./models/prontuario');
+const Usuario = require("./models/usuario");
 
 const router = Router();
 
@@ -22,213 +22,213 @@ router.get('/', async (req, res) =>{
 })
 
 
-// router.get("/tables", ensureAuthenticated, async (req, res) => {
-//   if (!isDatabaseConnected()) return res.status(502).json();
+router.get("/tables", ensureAuthenticated, async (req, res) => {
+  if (!isDatabaseConnected()) return res.status(502).json();
 
-//   console.log("GET /tables");
+  console.log("GET /tables");
 
-//   const sqlQuery = "SHOW TABLES";
-//   const [result, temp] = await database.query(sqlQuery);
+  const sqlQuery = "SHOW TABLES";
+  const [result, temp] = await database.query(sqlQuery);
 
-//   res.json(result);
-// });
+  res.json(result);
+});
 
-// router.get("/is-database-connected", ensureAuthenticated, async (req, res) => {
-//   res.json(await isDatabaseConnected());
-// });
+router.get("/is-database-connected", ensureAuthenticated, async (req, res) => {
+  res.json(await isDatabaseConnected());
+});
 
-// router.post("/create-dummy-data", ensureAuthenticated, async (req, res) => {
-//   if (!isDatabaseConnected()) return res.status(502).json();
+router.post("/create-dummy-data", ensureAuthenticated, async (req, res) => {
+  if (!isDatabaseConnected()) return res.status(502).json();
 
-//   console.log("POST /create-dummy-data");
+  console.log("POST /create-dummy-data");
 
-//   await createDummyData();
+  await createDummyData();
 
-//   return res.status(200).json();
-// });
+  return res.status(200).json();
+});
 
-// router.post("/new-record", ensureAuthenticated, async (req, res) => {
-//   if (!isDatabaseConnected()) return res.status(502).json();
+router.post("/new-record", ensureAuthenticated, async (req, res) => {
+  if (!isDatabaseConnected()) return res.status(502).json();
 
-//   console.log("POST /new-record");
+  console.log("POST /new-record");
 
-//   const {
-//     multiplicidade,
-//     entrada,
-//     saida,
-//     idade,
-//     genero,
-//     covid,
-//     gravidade,
-//     sintoma,
-//     comorbidade,
-//     obito,
-//     unidade
-//   } = req.body;
+  const {
+    multiplicidade,
+    entrada,
+    saida,
+    idade,
+    genero,
+    covid,
+    gravidade,
+    sintoma,
+    comorbidade,
+    obito,
+    unidade
+  } = req.body;
 
-//   console.log(req.body);
+  console.log(req.body);
 
-//   const paciente = await Paciente.create({
-//     idade: idade,
-//     genero: genero,
-//     multiplicidade: multiplicidade
-//   });
+  const paciente = await Paciente.create({
+    idade: idade,
+    genero: genero,
+    multiplicidade: multiplicidade
+  });
 
-//   const prontuario = await Prontuario.create({
-//     paciente_id: paciente.id,
-//     sintomas: sintoma,
-//     veioAObito: obito,
-//     transferencia: unidade,
-//     gravidade: gravidade,
-//     vacinadoCovid: covid,
-//     possuiComorbidades: comorbidade
-//   });
+  const prontuario = await Prontuario.create({
+    paciente_id: paciente.id,
+    sintomas: sintoma,
+    veioAObito: obito,
+    transferencia: unidade,
+    gravidade: gravidade,
+    vacinadoCovid: covid,
+    possuiComorbidades: comorbidade
+  });
 
-//   const atendimento = await Atendimento.create({
-//     horaEntrada: entrada,
-//     horaSaida: saida,
-//     paciente_id: paciente.id,
-//     prontuario_id: prontuario.id
-//   });
+  const atendimento = await Atendimento.create({
+    horaEntrada: entrada,
+    horaSaida: saida,
+    paciente_id: paciente.id,
+    prontuario_id: prontuario.id
+  });
 
-//   res.status(200).json();
-// });
+  res.status(200).json();
+});
 
-// router.get("/permanence", ensureAuthenticated, async (req, res) => {
-//   if (!isDatabaseConnected()) return res.status(502).json();
+router.get("/permanence", ensureAuthenticated, async (req, res) => {
+  if (!isDatabaseConnected()) return res.status(502).json();
 
-//   console.log("GET /permanence");
+  console.log("GET /permanence");
 
-//   // const query = "SELECT TIMESTAMPDIFF(MINUTE, horaEntrada, horaSaida) AS permanencia FROM atendimentos AS atendimento"
-//   // const [ data, temp ] = await database.query(query);
+  // const query = "SELECT TIMESTAMPDIFF(MINUTE, horaEntrada, horaSaida) AS permanencia FROM atendimentos AS atendimento"
+  // const [ data, temp ] = await database.query(query);
 
-//   const data = await Atendimento.findAll({
-//     attributes: [
-//       [
-//         database.fn(
-//           'TIMESTAMPDIFF',
-//           database.literal('MINUTE'),
-//           database.col('horaEntrada'),
-//           database.col('horaSaida'),
-//         ),
-//         'permanencia'
-//       ]
-//     ]
-//   });
+  const data = await Atendimento.findAll({
+    attributes: [
+      [
+        database.fn(
+          'TIMESTAMPDIFF',
+          database.literal('MINUTE'),
+          database.col('horaEntrada'),
+          database.col('horaSaida'),
+        ),
+        'permanencia'
+      ]
+    ]
+  });
 
-//   res.status(200).json(data);
-// });
+  res.status(200).json(data);
+});
 
-// router.get("/severity-and-permanence", ensureAuthenticated, async (req, res) => {
-//   if (!isDatabaseConnected()) return res.status(502).json();
+router.get("/severity-and-permanence", ensureAuthenticated, async (req, res) => {
+  if (!isDatabaseConnected()) return res.status(502).json();
 
-//   console.log("GET /severity-and-permanence");
+  console.log("GET /severity-and-permanence");
 
-//   // == QUERY: ==
-//   // SELECT TIMESTAMPDIFF(MINUTE, atendimentos.horaEntrada, atendimentos.horaSaida) AS permanencia,
-//   //        prontuarios.gravidade AS gravidade
-//   // FROM atendimentos
-//   // INNER JOIN prontuarios
-//   // ON atendimentos.prontuario_id = prontuarios.id
+  // == QUERY: ==
+  // SELECT TIMESTAMPDIFF(MINUTE, atendimentos.horaEntrada, atendimentos.horaSaida) AS permanencia,
+  //        prontuarios.gravidade AS gravidade
+  // FROM atendimentos
+  // INNER JOIN prontuarios
+  // ON atendimentos.prontuario_id = prontuarios.id
 
-//   const query = "SELECT TIMESTAMPDIFF(MINUTE, atendimentos.horaEntrada, atendimentos.horaSaida) AS permanencia, prontuarios.gravidade AS gravidade FROM atendimentos INNER JOIN prontuarios ON atendimentos.prontuario_id = prontuarios.id"
-//   const [data, temp] = await database.query(query);
+  const query = "SELECT TIMESTAMPDIFF(MINUTE, atendimentos.horaEntrada, atendimentos.horaSaida) AS permanencia, prontuarios.gravidade AS gravidade FROM atendimentos INNER JOIN prontuarios ON atendimentos.prontuario_id = prontuarios.id"
+  const [data, temp] = await database.query(query);
 
-//   res.status(200).json(data);
-// });
+  res.status(200).json(data);
+});
 
-// // ==== Login routes ====
+// ==== Login routes ====
 
-// router.post("/signup", async (req, res) => {
-//   console.log("POST /signup");
+router.post("/signup", async (req, res) => {
+  console.log("POST /signup");
 
-//   const { user, password, rootPassword } = req.body;
+  const { user, password, rootPassword } = req.body;
 
-//   // Check empty fields
-//   if (!user || !password || !rootPassword) {
-//     return res.status(400).json();
-//   }
+  // Check empty fields
+  if (!user || !password || !rootPassword) {
+    return res.status(400).json();
+  }
 
-//   // Check root password
-//   if (md5(rootPassword) != md5(env.ROOT_PASSWORD)) return res.status(401).json();
+  // Check root password
+  if (md5(rootPassword) != md5(env.ROOT_PASSWORD)) return res.status(401).json();
 
-//   // Check in database
-//   const userInDatabase = await Usuario.findOne({
-//     where: {
-//       username: user,
-//     }
-//   });
+  // Check in database
+  const userInDatabase = await Usuario.findOne({
+    where: {
+      username: user,
+    }
+  });
 
-//   // Check if user exists in database
-//   if (Boolean(userInDatabase)) return res.status(401).json()
+  // Check if user exists in database
+  if (Boolean(userInDatabase)) return res.status(401).json()
 
-//   // Create new user
-//   const newUser = await Usuario.create({
-//     username: user,
-//     password: md5(password),
-//   });
+  // Create new user
+  const newUser = await Usuario.create({
+    username: user,
+    password: md5(password),
+  });
 
-//   // Response with token
-//   const token = sign(
-//     {
-//       id: newUser.id,
-//       user: newUser.username,
-//     },
-//     env.JWT_SECRET,
-//     {
-//       subject: newUser.username,
-//       expiresIn: "1d"
-//     }
-//   );
+  // Response with token
+  const token = sign(
+    {
+      id: newUser.id,
+      user: newUser.username,
+    },
+    env.JWT_SECRET,
+    {
+      subject: newUser.username,
+      expiresIn: "1d"
+    }
+  );
 
-//   const response = {
-//     token: token,
-//     user: user,
-//   };
+  const response = {
+    token: token,
+    user: user,
+  };
 
-//   res.status(200).json(response);
-// });
+  res.status(200).json(response);
+});
 
-// router.post("/login", async (req, res) => {
-//   console.log("POST /login");
+router.post("/login", async (req, res) => {
+  console.log("POST /login");
 
-//   const { user, password } = req.body;
+  const { user, password } = req.body;
 
-//   // Check empty fields
-//   if (!user || !password) return res.status(400).json();
+  // Check empty fields
+  if (!user || !password) return res.status(400).json();
 
-//   // Check in database
-//   const userInDatabase = await Usuario.findOne({
-//     where: {
-//       username: user,
-//     }
-//   });
+  // Check in database
+  const userInDatabase = await Usuario.findOne({
+    where: {
+      username: user,
+    }
+  });
 
-//   // Check if user exists in database
-//   if (!Boolean(userInDatabase)) return res.status(404).json();
+  // Check if user exists in database
+  if (!Boolean(userInDatabase)) return res.status(404).json();
 
-//   // Check if passwords match
-//   if (md5(password) != userInDatabase.password) return res.status(401).json();
+  // Check if passwords match
+  if (md5(password) != userInDatabase.password) return res.status(401).json();
 
-//   // Response with token
-//   const token = sign(
-//     {
-//       id: userInDatabase.id,
-//       user: userInDatabase.username,
-//     },
-//     env.JWT_SECRET,
-//     {
-//       subject: userInDatabase.username,
-//       expiresIn: userInDatabase != env.ROOT_USERNAME ? "1d" : "10m",
-//     }
-//   );
+  // Response with token
+  const token = sign(
+    {
+      id: userInDatabase.id,
+      user: userInDatabase.username,
+    },
+    env.JWT_SECRET,
+    {
+      subject: userInDatabase.username,
+      expiresIn: userInDatabase != env.ROOT_USERNAME ? "1d" : "10m",
+    }
+  );
 
-//   const response = {
-//     token: token,
-//     user: user,
-//   }
+  const response = {
+    token: token,
+    user: user,
+  }
 
-//   res.status(200).json(response);
-// });
+  res.status(200).json(response);
+});
 
 module.exports = { router }
